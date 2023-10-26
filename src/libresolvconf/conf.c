843e46824a6d5802d3d8a41cade675f24098404d
Jan Hák <jan.hak@windowslive.com>
2023-10-26 20:30:52 +0200

    libresolvconf: refactoring of resolvconf.h

--- src/libresolvconf/conf.c
+++ src/libresolvconf/conf.c
@@ -18,7 +18,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "resolvconf.h"
+#include "conf.h"
 
 #include <fcntl.h>
 #include <resolv.h>
--- src/libresolvconf/include/libresolvconf/resolvconf.h
+++ src/libresolvconf/include/libresolvconf/resolvconf.h
@@ -20,56 +20,6 @@
 
 #pragma once
 
-#include <stddef.h>
-#include <stdint.h>
-
-#include "error.h"
 #include "arr_str.h"
-
-#define LRESCONF_FAMILY_LEN 2
-#define LRESCONF_LOOKUP_LEN 2
-
-typedef enum {
-	IRESCONF_LOOKUP_UNSPEC = 0,
-	IRESCONF_LOOKUP_BIND = 1,
-	IRESCONF_LOOKUP_FILE = 2
-} __attribute__ ((__packed__)) lresconf_lookup_t;
-
-typedef struct {
-	lresconf_arr_cstr_t nameservers;
-	lresconf_arr_cstr_t domains;
-	lresconf_arr_cstr_t sortlist;
-	int family[LRESCONF_FAMILY_LEN];
-	lresconf_lookup_t lookup[LRESCONF_LOOKUP_LEN];
-	struct {
-		uint8_t attempts;
-		uint8_t timeout;
-		uint8_t ndots;
-		bool    debug                 : 1;
-		bool    edns0                 : 1;
-		bool    inet6                 : 1;
-		bool    insecure1             : 1;
-		bool    insecure2             : 1;
-		bool    ip6_bytestring        : 1;
-		bool    ip6_dotint            : 1;
-		bool    no_check_names        : 1;
-		bool    no_reload             : 1;
-		bool    no_tld_query          : 1;
-		bool    rotate                : 1;
-		bool    single_request        : 1;
-		bool    single_request_reopen : 1;
-		bool    tcp                   : 1;
-		bool    trust_ad              : 1;
-		bool    use_vc                : 1;
-	} options;
-	struct {
-		size_t line;
-		size_t col;
-	} error;
-} lresconf_conf_t;
-
-void lresconf_conf_deinit(lresconf_conf_t *conf);
-
-int lresconf_load_defaults(lresconf_conf_t *conf);
-int lresconf_load_file(lresconf_conf_t *conf, const char *path);
-int lresconf_load_env(lresconf_conf_t *conf);
+#include "conf.h"
+#include "error.h"
